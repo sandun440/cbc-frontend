cbb8a415d501e139735881d13ea3b296b24ce899
sandun440 <sameerasandun440@gmail.com>
2025-03-15 00:02:09 +0530

    logo fix

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return(
         <header className="bg-white w-full h-[80px] relative flex items-center justify-center">
-            <img src="/public/logo.png" className="absolute h-full rounded-full cursor-pointer left-[10px]" />
+            <img src="logo.png" className="absolute h-full rounded-full cursor-pointer left-[10px]" />
 
             <div className="h-full w-[500px]  flex items-center justify-between">
                 <Link to="/" className="text-accent font-bold text-xl hover:border-b border-b-accent">Home</Link>
