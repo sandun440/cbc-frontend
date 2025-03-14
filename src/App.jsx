22b721b3390d74eb5f13822143c07a275653f893
sandun440 <sameerasandun440@gmail.com>
2025-03-14 22:28:30 +0530

    remove impot home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import AdminHomePage from './pages/AdminHomePage'
